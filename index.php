fa4510e1daf328d0c545ab1b724a4abacf0f37f1
BANKA2017 <kdwnil@hotmail.com>
2017-01-08 16:51:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header("Content-type: text/html; charset=utf-8");
 ignore_user_abort(true);
 $url = 'https://tieba.baidu.com/mo/q/apubpost';
-$t = file_get_contents("./twitter.php?name=LoveLive_staff&id=0");
+$t = file_get_contents("./twitter.php?name=LoveLive_staff&id=0");//别忘了改这里
 $tbs='';//tbs这不用我说了吧
 $fid='';//贴吧对应的fid
 $tid='';//帖子对应的tid
