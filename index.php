a4691c52f7d06ce045f5d6a116b20978b0b7d37e
BANKA2017 <kdwnil@hotmail.com>
2017-01-14 23:02:36 +0800

    Update index.php
    fix

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $ch = curl_init($url);
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36');
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch,CURLOPT_COOKIE,$cookie);
-curl_setopt($ch, CURLOPT_REFERER,'https://tieba.baidu.com/p/$tid?pn=0&');
+curl_setopt($ch, CURLOPT_REFERER,'https://tieba.baidu.com/p/'.$tid.'?pn=0&');
 curl_setopt($ch, CURLOPT_POST,1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
 $content = curl_exec($ch);
