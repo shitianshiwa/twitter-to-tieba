50f9a37fb15f415e1ed673357eaf953c2ce8ce38
yaoyichi2011 <yaoyichi2011@users.noreply.github.com>
2017-02-26 13:11:29 +0800

    Revert "v3.1"

--- index.php
+++ index.php
@@ -14,21 +14,24 @@ $tbsjson = curl_exec($tbscurl);
 curl_close($tbscurl);
 $tbs=json_decode($tbsjson,1)["tbs"];
 $twurl = 'https://mobile.twitter.com/'.$name;
-$api=file_get_contents($twurl);
-preg_match_all('/<div[^>]*?>(.[\s\S]*?)<\/div>/u',$api,$kd1);
-preg_match_all('/<div class="tweet-text" data-id="(.+?)">/u',$api,$kd2);
-echo $kd1[1][8];
-echo $kd2[1][0];
+$zh = curl_init($twurl);
+curl_setopt($zh,CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36');
+curl_setopt($zh,CURLOPT_RETURNTRANSFER,1);
+$api = curl_exec($zh);
+curl_close($zh);
+preg_match('/<script type(.+?)>(.+?)<\/script>/ies',$api,$json);
 $b=json_decode($json[2],1);
-if(strlen($b["state"]["timeline"]["items"][0]["data"]["tweet"]["inlineMedia"]["mediaDetails"]["imageUrl"])>0){
 $pic='图片地址:'.$b["state"]["timeline"]["items"][0]["data"]["tweet"]["inlineMedia"]["mediaDetails"]["imageUrl"];
-}else{$pic='';}
-$text=str_replace("twitter"," * ",str_replace("goo.gl"," * ",str_replace("@","@ ",str_replace("\n"," ",str_replace("&"," ",str_replace(">"," ",str_replace("<"," ",str_replace("'"," ",str_replace('"'," ",strip_tags($kd1[1][8]))))))))));
-$tweetid=$kd2[1][0];
+$from=$b["state"]["pageData"]["title"];
+$text=$b["state"]["timeline"]["items"][0]["data"]["tweet"]["text"]["textString"];
+$textold=str_replace("t.co","*",str_replace("goo.gl","*",str_replace("@","@ ",str_replace("\n"," ",str_replace("&"," ",str_replace(">"," ",str_replace("<"," ",str_replace("'"," ",str_replace('"'," ",$b["state"]["timeline"]["items"][0]["data"]["tweet"]["text"]["textString"])))))))));
+$time=$b["state"]["timeline"]["items"][0]["data"]["tweet"]["timestamp"];
+$tweetid=$b["state"]["timeline"]["items"][0]["data"]["tweet"]["id"];
 $check= file($name.'.txt')[0];
 if($tweetid>$check){
 $url = 'https://tieba.baidu.com/mo/q/apubpost';
-if(strlen($text)>0){
+$t = $from.':'.$textold.' '.$time;
+if(strlen($t)>0){
 $getlanguage='text_to_translate='.$text;
 $gl = curl_init('https://www.translate.com/translator/ajax_lang_auto_detect');
 curl_setopt($gl,CURLOPT_RETURNTRANSFER,1);
@@ -48,10 +51,9 @@ $bdback = curl_exec($bdtr);
 curl_close($bdtr);
 $translate=json_decode($bdback,1)["translated_text"];
 if(strlen($translate)>0){
-$data = 'co='.$name.':'.$text.'
-翻译:'.str_replace("@","@ ",str_replace("t.co"," * ",str_replace("goo.gl","*",str_replace("\n"," ",str_replace("&"," ",str_replace(">"," ",str_replace("<"," ",str_replace("'"," ",str_replace('"',' ',$translate))))))))).'
-'.$pic.'
-&_t=1484059982471&tag=11&upload_img_info='.${$name}.'&fid='.$fid.'&src=1&word='.$tbn.'&tbs='.$tbs.'&z='.$tid;
+$data = 'co='.$t.'
+翻译:'.str_replace("t.co","*",str_replace("goo.gl","*",str_replace("\n"," ",str_replace("&"," ",str_replace(">"," ",str_replace("<"," ",str_replace("'"," ",str_replace('"',' ',$translate)))))))).'
+'.$pic.&upload_img_info=&fid='.$fid.'&src=1&word='.$tbn.'&tbs='.$tbs.'&z='.$tid;
 $ch = curl_init($url);
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Mobile Safari/537.36');
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
