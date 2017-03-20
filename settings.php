311abe27c83cb9d905e5907d4d3f96c81d945d2e
WangLingLing <n00099@126.com>
2017-03-20 23:26:30 +0800

    Delete settings.php

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?
-$cookie='';
-/*兼容吧务申请插件*/
-$realname = urlencode('');//真实姓名
-$province =urlencode('');//所在省份
-$city = urlencode('');//所在城市
-$site = urlencode('');//所在地区(联系地址，不必很详细)
-$im = urlencode('');//qq号码(有需要可以自己改配置)
-$declaration = urlencode('');//申请理由
