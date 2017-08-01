520df938c34b7b6c8015c893dba7fe1d95d372d0
yaoyichi2011 <yaoyichi2011@users.noreply.github.com>
2017-08-01 16:59:22 +0800

    beta2
    增加bing翻译源文字输出优化

--- index.php
+++ index.php
@@ -3,35 +3,40 @@ header("Content-type: text/html; charset=utf-8");
 ignore_user_abort(true);
 require dirname(__FILE__).'/settings.php';
 require dirname(__FILE__).'/scurl.php';
-$name=urlencode($argv[1]);
-$tid=$argv[2];
-$tbn=urlencode($argv[3]);
+$name=urlencode($_GET['name']);//urlencode($argv[1]);
+$tid=$_GET['tid'];//$argv[2];
+$tbn=urlencode(($_GET['tbn']));//urlencode($argv[3]);
 $fid=json_decode(file_get_contents('http://tieba.baidu.com/f/commit/share/fnameShareApi?ie=utf-8&fname='.$tbn),1)["data"]["fid"];
-$tbs=json_decode(scurl ('http://tieba.baidu.com/dc/common/tbs','','',$cookie,''),1)["tbs"];
+$tbs=json_decode(scurl('http://tieba.baidu.com/dc/common/tbs','','',$cookie,''),1)["tbs"];
 $api=file_get_contents('https://mobile.twitter.com/'.$name);
 preg_match_all('/<div[^>]*?>(.[\s\S]*?)<\/div>/u',$api,$kd1);
 preg_match_all('/<div class="tweet-text" data-id="(.+?)">/u',$api,$kd2);
-$pic='';
-$text=urlencode(str_replace("twitter"," * ",str_replace("goo.gl"," * ",str_replace("@","@ ",strip_tags($kd1[1][8])))));
+$text=chop(str_replace("twitter"," * ",str_replace("goo.gl"," * ",str_replace("@","@ ",strip_tags($kd1[1][8])))));
 $tweetid=$kd2[1][0];
 $check=file($name.'.txt')[0];
 if ($tweetid>$check){
 	if (strlen($text)>0){
-		$data=str_replace("10;","",'co='.$name.':'.htmlentities($text).$pic.'&_t=1484059982471&tag=11&upload_img_info=&fid='.$fid.'&src=1&word='.$tbn.'&tbs='.$tbs.'&z='.$tid);
-		$a=json_decode(scurl ('https://tieba.baidu.com/mo/q/apubpost',1,$data,$cookie,'http://tieba.baidu.com/p/'.$tid.'?pn=0&'),1);
-		$log=fopen('log.txt',"a");
-		flock($log,LOCK_EX);
-		fwrite($log,'['.time().','.json_encode($a).','.$name.','.urldecode($text).','.$translate.']'."\r\n");
-		fclose($log,LOCK_UN);
-		if ($a['no']>"0"){
-			echo "失败！";
+		$language=json_decode(scurl('https://www.translate.com/translator/ajax_lang_auto_detect',1,'text_to_translate='.urlencode($text),'','https://www.translate.com/'),1)["language"];
+		$translate=json_decode(scurl('https://www.translate.com/translator/ajax_translate',1,'text_to_translate='.urlencode($text).'&source_lang='.$language.'&translated_lang=zh&use_cache_only=false','','https://www.translate.com/'),1)["translated_text"];
+		if (strlen($translate)>0){
+			$data='co='.$name.':'.htmlentities(urlencode($text))."\n".'翻译:'.urlencode($translate).'&_t=1484059982471&tag=11&upload_img_info=&fid='.$fid.'&src=1&word='.$tbn.'&tbs='.$tbs.'&z='.$tid;
+			$a=json_decode(scurl('https://tieba.baidu.com/mo/q/apubpost',1,$data,$cookie,'http://tieba.baidu.com/p/'.$tid.'?pn=0&'),1);
+			$log=fopen('log.txt',"a");
+			flock($log,LOCK_EX);
+			fwrite($log,'['.time().','.json_encode($a).','.$name.','.$text.','.$translate.']'."\r\n");
+			fclose($log,LOCK_UN);
+			if ($a['no']>"0"){
+				echo "失败！";
+			}else {
+				echo '发送成功';
+			}
+			$fp=fopen($name.'.txt',"w");
+			flock($fp,LOCK_EX);
+			fwrite($fp,$tweetid);
+			fclose($fp,LOCK_UN);
 		}else {
-			echo '发送成功';
+			echo '翻译字数不能为0';
 		}
-		$fp=fopen($name.'.txt',"w");
-		flock($fp,LOCK_EX);
-		fwrite($fp,$tweetid);
-		fclose($fp,LOCK_UN);
 	}else {
 		echo '字数不能为0';
 	}
