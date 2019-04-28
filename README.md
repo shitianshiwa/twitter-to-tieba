e446039ecb6a4777bea93cf4c9e17bc757bac9bb
n0099 <n@n0099.net>
2019-04-28 19:09:08 +0800

    * tidy up codes, plz comply with PSR-1/2 standards * renamed many variable to improve code readability + rollback from hardcoded configures to http get (use php-cgi to simulate) configure - remove useless stdout(echo) reply logs

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
-# twitter-to-tieba
-最新版本:5.0
-----
-把指定用户的推特转发到贴吧
-现已支持选择/随机回贴客户端（WAP、~~iPhone~~、~~安卓~~、~~WP~~、~~Win8UWP~~），回贴失败自动重发（被吞不算），[Google翻译（机翻不完美）](https://github.com/statickidz/php-google-translate-free)，转推（支持转任何推特用户的推到任何贴吧的任何帖子里面，主要功能），自动写入回贴log，无需数据库，使用json csv存储（数据库版待开发），~~检查cookie完整性~~
-**发起**: [BANKA2017](https://ailand.date)
-**主程**: [n0099 四叶重工](https://n0099.net)
-**使用**
-填写`index.php` 7~12行内容，运行`php index.php`
-**环境要求**
-- php
-- php-curl
+# twitter-to-tieba v5.0
+将指定用户的推特自动转发到贴吧
 
-**警告**
+## 功能
+* 主要功能转推：将任意推特用户的转发到贴吧的主题贴下
+* [推文Google翻译（机翻不完美），使用此库](https://github.com/statickidz/php-google-translate-free)
+* 选择/随机回贴客户端（WAP、iPhone、安卓、WP、Win8UWP）
+* 回贴失败自动重发（无法检测被吞/删），回贴与推文log
+* 无需数据库仅本地存储（使用json+csv存储log）
+
+**发起：**[BANKA2017](https://ailand.date)
+**主程：**[n0099 四叶重工](https://n0099.net)
+
+## 警告
 使用本脚本可能会导致贴吧账号被永久封禁
-**效果**：
+## 效果
 ![](https://kdnetwork.github.io/api/images/twtotb1.png)
+## 使用
+1. 填写`t2t.php` 8~13行用户配置项
+2. 执行命令或设置crontab
+```
+php-cgi t2t.php
+username=发贴用户名，用户参数此前必须已在t2t.php中配置过
+tieba=转发主题贴所在贴吧名称
+tid=转发主题贴tid
+twitter=推特账号id或话题名称（带前置#）
+using_translate=是否使用谷歌机翻推文，取值true或false
+```
+3. 可设置多条命令以配置多套转发参数
+## 环境需求
+- php7
+- php-curl
--- t2t.php
+++ t2t.php
@@ -1,24 +1,29 @@
 <?php
 ini_set('display_errors', 'On');
-require 'src/GoogleTranslate.php';
+require_once './GoogleTranslate.php';
 use \Statickidz\GoogleTranslate;
-$_GET = [];
 
-// 百度账号BDUSS Cookie
-$tieba_bduss = '';
-//其他设置
-$_GET['tieba'] = "";//贴吧名称
-$_GET['twitter'] = "";//urlencode($_GET['twitter']);//*用户id或话题 **话题前必须带'#'
-$_GET['tid'] = 0;//(int)($_GET['tid']);//帖子tid
+$tieba_users = [
+    // 第一个百度账号配置，扩充请复制
+    '用户名' => [ /*用户名，仅供标示用不需要是百度ID*/
+        'bduss' => '', // 百度账号BDUSS Cookie
+        // 回贴客户端 0:WAP|1:iPhone|2:Android|3:WindowsPhone|4:Windows8UWP
+        // 随机或固定客户端二选一
+        'reply_device' = rand(0, 4) // 随机
+        //'reply_device' = 0 // 固定
+    ],
+];
 
-// 是否使用谷歌翻译
-$use_Google_Translate = true;
-$reply_devices = 0;//客户端待处理
-//// 回贴客户端 0:WAP|1:iPhone|2:Android|3:WindowsPhone|4:Windows8UWP 默认随机
-//$reply_devices = rand(0, 4);
-//// iPhone回贴秒吞
-//$reply_devices = $reply_devices == 1 ? rand(2, 4) : $reply_devices;
-function post_reply($bduss, $tbs, $devices, $tid, $fid, $tieba, $content) {
+$_GET['username'] = (string)$_GET['username'];
+$_GET['tieba'] = (string)$_GET['tieba']; // 贴吧名称
+$_GET['tid'] = (int)$_GET['tid']; //帖子tid
+$_GET['twitter'] = urlencode($_GET['twitter']); //*用户id或话题 **话题前必须带'#'
+$_GET['using_translate'] = (bool)$_GET['using_translate'];
+$tieba_user = $tieba_users[$_GET['username']];
+
+$curl_mobile_device_ua = 'Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36';
+
+function post_reply(string $bduss, string $tbs, int $devices, int $tid, int $fid, string $tieba, string $content): string {
     if ($devices == 0) {
         // WAP回贴
         $post_data = [
@@ -31,6 +36,7 @@ function post_reply($bduss, $tbs, $devices, $tid, $fid, $tieba, $content) {
         ];
         $curl = curl_init('http://tieba.baidu.com/mo/q/apubpost');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, $curl_mobile_device_ua);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post_data));
         curl_setopt($curl, CURLOPT_COOKIE, "BDUSS={$bduss};");
         return json_decode(curl_exec($curl), true);
@@ -40,7 +46,7 @@ function post_reply($bduss, $tbs, $devices, $tid, $fid, $tieba, $content) {
             'BDUSS' => $bduss,
             '_client_id' => 'wappc_136'.mt_rand(1000000000, 9999999999).'_'.mt_rand(100, 999),
             '_client_type' => $devices,
-            '_client_version' => '6.5.2',
+            '_client_version' => '8.8.8',
             '_phone_imei' => md5($bduss),
             'anonymous' => 0,
             'content' => $content,
@@ -51,12 +57,14 @@ function post_reply($bduss, $tbs, $devices, $tid, $fid, $tieba, $content) {
             'tid' => $tid,
             'title' => ''
         ];
+        // tieba client sign
         foreach ($post_data as $key => $value) {
             $sign .= "{$key}={$value}";
         }
         $post_data['sign'] = strtoupper(md5($sign.'tiebaclient!!!'));
         $curl = curl_init('http://c.tieba.baidu.com/c/c/post/add');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, $curl_mobile_device_ua);
         curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post_data));
         curl_setopt($curl, CURLOPT_COOKIE, "BDUSS={$bduss};");
@@ -64,84 +72,116 @@ function post_reply($bduss, $tbs, $devices, $tid, $fid, $tieba, $content) {
     }
 }
 
-function get_tieba_fid($forum) {
+function get_tieba_fid(string $forum): int {
     return json_decode(file_get_contents("http://tieba.baidu.com/f/commit/share/fnameShareApi?ie=utf-8&fname={$forum}"), true)['data']['fid'];
 }
 
-function get_tbs($bduss) {
+function get_tbs(string $bduss): string {
     $curl = curl_init('http://tieba.baidu.com/dc/common/tbs');
     curl_setopt($curl, CURLOPT_COOKIE, "BDUSS={$bduss};");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     return json_decode(curl_exec($curl), true)['tbs'];
 }
-function get_from_mobile_twitter($url, $type = 'get', $header = [], $post_data = null){
+
+function get_from_mobile_twitter(string $url, string $type = 'get', array $header = [], $post_data = null) {
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36');
+    curl_setopt($curl, CURLOPT_USERAGENT, $curl_mobile_device_ua);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if($type == 'post'){
+    if ($type == 'post') {
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
     }
     return curl_exec($curl);
 }
-//内容锁，判断是否有更新
-if(is_file("{$_GET['twitter']}_posted.csv"))
-    $tweets_posted = str_getcsv(file_get_contents("{$_GET['twitter']}_posted.csv"));
-else
-    $tweets_posted = [0];
-//判断是否为话题
-$is_topic = substr($_GET["twitter"], 0, 1) == '#' ? true : false;
-//追踪是否有更新
-if ($is_topic) {
-    $check_update = json_decode(file_get_contents("https://twitter.com/i/search/timeline?q=" . urlencode($_GET["twitter"]) . "&latent_count=1&min_position={$tweets_posted[0]}"), true);
+
+// 推文锁，判断推文是否已被发送过
+if (is_file("{$_GET['twitter']}_posted.csv")) {
+    $tweets_posted = str_getcsv(file_get_contents("{$_GET['twitter']}_posted.csv")); // possible OOM, should only read tail lines
 } else {
-    $check_update = json_decode(file_get_contents('https://twitter.com/i/profiles/show/' . $_GET["twitter"] . '/timeline/tweets?composed_count=0&include_available_features=0&include_entities=0&include_new_items_bar=true&interval=30000&latent_count=0&min_position=' . $tweets_posted[0]), true);
+    $tweets_posted = [0];
 }
-if($check_update["new_latent_count"] > 0){
-    $tbs = get_tbs($tieba_bduss);
-    $fid = get_tieba_fid($_GET['tieba']);
-    preg_match('/gt=([0-9]*);/', get_from_mobile_twitter('https://mobile.twitter.com/'), $get_gt);
-    if($is_topic){
-        $user_info = json_decode(get_from_mobile_twitter('https://api.twitter.com/graphql/GpQevDzQ0VLTV4-68vrePA', 'post', ["authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA", "content-type: application/json", "x-guest-token: " . $get_gt[1]], json_encode(["variables" => json_encode(["screen_name" => $_GET["twitter"], "withHighlightedLabel" => true]), "queryId" => "GpQevDzQ0VLTV4-68vrePA"])), true);
-        $tweets = json_decode(get_from_mobile_twitter('https://api.twitter.com/2/timeline/profile/' . $user_info["data"]["user"]["rest_id"] . '.json?' . $user_info["data"]["user"]["rest_id"] . '&count=' . $check_update, 'get', ["authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA", "x-guest-token: " . $get_gt[1]]), true);
-    }else{
-        $tweets = json_decode(get_from_mobile_twitter("https://api.twitter.com/2/search/adaptive.json?q=" . urlencode($_GET["twitter"]) . "&count=20&query_source=hash&pc=1", 'get', ["authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA", "x-guest-token: " . $get_gt[1]]), true);
+
+// 推特id是否为话题
+$tw_is_topic = substr($_GET["twitter"], 0, 1) == '#' ? true : false;
+// 推文是否有更新
+if ($tw_is_topic) {
+    $tw_update_check_json = json_decode(file_get_contents('https://twitter.com/i/search/timeline?q=' . urlencode($_GET["twitter"]) . "&latent_count=1&min_position={$tweets_posted[0]}"), true);
+} else {
+    $tw_update_check_json = json_decode(file_get_contents("https://twitter.com/i/profiles/show/{$_GET["twitter"]}timeline/tweets?composed_count=0&include_available_features=0&include_entities=0&include_new_items_bar=true&interval=30000&latent_count=0&min_position={$tweets_posted[0]}"), true);
+}
+if ($tw_update_check_json["new_latent_count"] > 0) {
+    preg_match('/gt=([0-9]*);/', get_from_mobile_twitter('https://mobile.twitter.com/'), $tw_guest_token);
+    $tw_curl_auth_header = 'authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA';
+    if ($tw_is_topic) {
+        $tw_user_info = json_decode(get_from_mobile_twitter(
+            'https://api.twitter.com/graphql/GpQevDzQ0VLTV4-68vrePA',
+            'post',
+            [
+                $tw_curl_auth_header,
+                'content-type: application/json',
+                "x-guest-token: {$tw_guest_token[1]}"
+            ],
+            json_encode([
+                'variables' => json_encode(["screen_name" => $_GET["twitter"], "withHighlightedLabel" => true]),
+                'queryId' => 'GpQevDzQ0VLTV4-68vrePA'
+            ])
+        ), true);
+        $tweets = json_decode(get_from_mobile_twitter(
+            "https://api.twitter.com/2/timeline/profile/{$tw_user_info['data']['user']['rest_id']}.json?{$tw_user_info['data']['user']['rest_id']}&count={$tw_update_check_json["new_latent_count"]}",
+            'get',
+            [
+                $tw_curl_auth_header,
+                "x-guest-token: {$tw_guest_token[1]}"
+            ]
+        ), true);
+    } else {
+        $tweets = json_decode(get_from_mobile_twitter(
+            'https://api.twitter.com/2/search/adaptive.json?q=' . urlencode($_GET["twitter"]) . '&count=20&query_source=hash&pc=1',
+            'get',
+            [
+                $tw_curl_auth_header,
+                "x-guest-token: {$tw_guest_token[1]}"
+            ]
+        ), true);
     }
+    
+    $tieba_tbs = get_tbs($tieba_user['bduss']);
+    $tieba_fid = get_tieba_fid($_GET['tieba']);
     foreach ($tweets["globalObjects"]["tweets"] as $tweet_id => $tweet) {
-        //$tweet_id =  $tweet[1];//tweets id
-        $tweet_content = $tweet["text"];//文字内容
-        //好了好了，现在支持了// 不支持推特话题链接
-        $tweet_content = str_replace(["https://t.co/[0-9a-zA-Z]{10}"], '', $tweet_content); // 去除链接
         if (in_array($tweet_id, $tweets_posted) == false) {
-            $translate_result = $use_Google_Translate == true ? (new GoogleTranslate) -> translate('auto', 'zh-CN', $tweet_content) : null;
-        echo "Should post reply about Tweet({$tweet_id}) with content {$tweet_content} on tieba forum {$_GET['tieba']} with thread id {$_GET['tid']} via wap post port" . (isset($translate_result) ? ', using Google Translate Service' : null) ."\r\n";
-            $reply_content = "用户名：@{$_GET['twitter']}\r\n推文：{$tweet_content}\r\n" . (isset($translate_result) ? "翻译：{$translate_result}\r\n" : null) . "推文链接：https://twitter.com/{$_GET['twitter']}/status/{$tweet_id}";//将要发送到贴吧的文字
-            $reply_result = post_reply($tieba_bduss, $tbs, $reply_devices, $_GET['tid'], $fid, $_GET['tieba'], $reply_content);
-            if ($reply_result[$reply_devices == 0 ? 'no' : 'error_code'] == 0) {
-                echo 'successful posted';
-                $tweet_log = fopen("{$_GET['twitter']}_posted.csv", 'a');//这个是锁
-                flock($tweet_log, LOCK_EX);
-                fwrite($tweet_log, "\"{$tweet_id}\",\r\n");
+            $tweet_content = $tweet["text"]; // 文字内容
+            // 好了好了，现在支持了 2019-04-25 // 不支持推特话题链接 2017
+            $tweet_content = str_replace(["https://t.co/[0-9a-zA-Z]{10}"], '', $tweet_content); // 去除链接
+            $translated_result = $_GET['using_translate'] == true ? (new GoogleTranslate)->translate('auto', 'zh-CN', $tweet_content) : null;
+
+            $reply_content = "ID：{$_GET['twitter']}\n推文：{$tweet_content}\n" . (isset($translated_result) ? "翻译：{$translated_result}\n" : null) . "链接：https://twitter.com/{$_GET['twitter']}/status/{$tweet_id}"; // 回帖内容
+            $reply_result = post_reply($tieba_user['bduss'], $tieba_tbs, $tieba_user['reply_device'], $_GET['tid'], $tieba_fid, $_GET['tieba'], $reply_content);
+
+            if ($reply_result[$tieba_user['reply_device'] == 0 ? 'no' : 'error_code'] == 0) {
+                // 成功回复后打推文log
+                $tweet_log = fopen("{$_GET['twitter']}_posted.csv", 'a');
+                flock($tweet_log, LOCK_EX); // 
+                fwrite($tweet_log, "\"{$tweet_id}\",\n"); // csv format
                 fclose($tweet_log);
-            } else {
-                echo "reply failed, error code:{$reply_result[$reply_devices == 0 ? 'no' : 'error_code']}, reason: {$reply_result[$reply_devices == 0 ? 'error' : 'msg']}";
             }
-            //这个是log
+
+            // 打回复log
             $reply_log = [
                 'reply_time' => date(DATE_ISO8601),
                 'twitter_name' => $_GET['twitter'],
                 'tweet_id' => $tweet_id,
                 'tweet_content' => str_replace(["\r\n", "\n"], '', $tweet_content),
                 'reply_content' => $reply_content,
-                'reply_devices' => $reply_devices,
+                'reply_device' => $tieba_user['reply_device'],
                 'reply_result' => implode(' ', $reply_result)
             ];
             $reply_log_file = fopen('reply_log.json', 'a');
             flock($reply_log_file, LOCK_EX);
-            fwrite($reply_log_file, json_encode($reply_log, JSON_UNESCAPED_UNICODE) . "\r\n");
+            fwrite($reply_log_file, json_encode($reply_log, JSON_UNESCAPED_UNICODE) . "\n");
             fclose($reply_log_file);
-            sleep(1);
+
+            sleep(1); // reply interval 1s
         }
     }
 }
